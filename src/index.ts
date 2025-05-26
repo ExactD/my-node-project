733d99cd05a189e02605ed2785f6540c4d4fb354
ExactD <nazarcukmihalo9@gmail.com>
2025-05-26 10:23:44 +0300

    Додано дозвіл на підключення до API

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,19 @@ const pool = new Pool({
   port: Number(process.env.DB_PORT),
 });
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); // Відповідь на preflight
+  }
+
+  next();
+});
+
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
