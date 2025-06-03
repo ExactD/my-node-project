55e5a6d86386c7dc8f0be583904cde103b329ea7
ExactD <nazarcukmihalo9@gmail.com>
2025-06-03 21:05:34 +0300

    Фікс 6

--- src/app.ts
+++ src/app.ts
@@ -59,6 +59,15 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 if (!process.env.EMAIL_USER || !process.env.EMAIL_PASSWORD) {
   console.warn('EMAIL_USER або EMAIL_PASSWORD не встановлені в .env файлі. В режимі розробки коди будуть показуватись у консолі.');
 }
