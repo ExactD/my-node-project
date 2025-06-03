af610f66037887d5cef5387a033da2146b15e5ef
ExactD <nazarcukmihalo9@gmail.com>
2025-06-03 21:21:58 +0300

    Фікс 7

--- src/test.ts
+++ src/test.ts
@@ -35,15 +35,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  if (req.method === 'OPTIONS') return res.sendStatus(200);
-  next();
-});
-
 
 // Middleware для перевірки JWT
 const authenticateToken = (req: any, res: Response, next: any) => {
